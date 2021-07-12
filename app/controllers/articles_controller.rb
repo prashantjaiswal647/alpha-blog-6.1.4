f0d81b4af5f9194fedcebec9ee9ce9cb88e44685
Tristan Lee <leetristan2000@gmail.com>
2021-07-12 03:17:34 -0700

    Article CRUD actions added, Show, Edit, Delete, & Create buttons added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,4 +7,40 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
 
+  def new
+    @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :description)) # This makes an article by requiring the inputs with params, but only permitting :title and :description hash keys to come through, as a security feature. It used to be params[:article]
+    # render plain: @article.inspect # To see the creation for testing purposes
+    if @article.save
+      flash[:notice] = "Article was created successfully." # There are two types of flash, notice and alert. Alert is usually for when something goes wrong. This is to show that the article was successfully created.
+      redirect_to @article # Below is longhand, this is shorthand since this is so commonly used.
+      # redirect_to article_path(@article) # use rails routes --expanded in console, go up to the show action, then see that the prefix is 'article', meaning article_path, and that it needs the ID, so we give @article which Rails knows to get the ID of the article given, then to redirect_to it and show that view.
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title, :description))
+      flash[:notice] = "Article was updated successfully."
+      redirect_to @article # Send to the article
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path # Send to the 'index' table of Articles
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
-  resources :articles, only: [:show, :index]
+  resources :articles #, only: [:show, :index, :new, :create, :edit, :update, :destroy] # We slowly added all of the routes in one by one to learn about them. resources :articles has all of these, so having them all listed is no longer necessary.
 end
