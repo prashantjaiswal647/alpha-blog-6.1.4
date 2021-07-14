cb8294ef763f80f5b5feba35e337020163304854
Tristan Lee <leetristan2000@gmail.com>
2021-07-14 04:01:07 -0700

    Add user validations, email regex

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
-
+  validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 25 } # Check that username exists (presence) and has a minimum length of 3 and a maximum length of 25 characters.
+  VALID_EMAIL_REGEX = /\A[^\._-][\w\.-]+[^\.]@[^\.][a-z\d\.-]+\.[a-z]+\z/i
+  validates :email, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 105 }, format: { with: VALID_EMAIL_REGEX }
 end
