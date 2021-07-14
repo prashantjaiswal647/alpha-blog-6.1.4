9ecc491620c2da6e14d08e62a8a3d816071329e0
Tristan Lee <leetristan2000@gmail.com>
2021-07-14 04:19:16 -0700

    Added before_save to change email to downcase

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  before_save { self.email = email.downcase }
   has_many :articles # Plural since a user can create many articles
   validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 25 } # Check that username exists (presence) and has a minimum length of 3 and a maximum length of 25 characters.
   VALID_EMAIL_REGEX = /\A[^\._-][\w\.-]+[^\.]@[^\.][a-z\d\.-]+\.[a-z]+\z/i
